0e659f14485e356a77390961071a7abd069259f7
Sameer Giri <optimusam@users.noreply.github.com>
2017-01-29 04:48:12 +0530

    added code to store all blocks RGB
    and some indentation to make it human-friendly

--- color.js
+++ color.js
@@ -2,44 +2,33 @@ var blocks = document.querySelectorAll(".square");
 var rField = document.getElementById("r");
 var gField = document.getElementById("g");
 var bField = document.getElementById("b");
-var result = document.querySelector("h2");
-var count = 0;
-var aRed, aBlue, aGreen, red, green, blue;
-var correctColor = Math.floor(Math.random() * 7);
-var isCorrect;
+var result = document.querySelector("h2"); //to show wrong or right
+var aRed, aBlue, aGreen, red, green, blue; //a<Color> refers to answer color
+var correctColor = Math.floor(Math.random() * 6); // storing correct block number
+var isCorrect=false;
+var data = []; // to store all the blocks rgb color as objects
 
 function changeBackground(tile)
-{  
+{
     // generating random colors using Math.random() * (max - min) + min
     red = Math.floor(Math.random() * 256);
     green = Math.floor(Math.random() * 256);
     blue = Math.floor(Math.random() * 256);
-    tile.style.background = "#"+(red).toString(16)+(green).toString(16)+(blue).toString(16);
-    if(correctColor===count)
-        {
-            rField.textContent=red;
-            gField.textContent=green;
-            bField.textContent=blue;
-            aRed =red;
-            aBlue=blue;
-            aGreen=green;
-        }
-    count+=1;
+    tile.style.background = "#"+(red).toString(16)+(green).toString(16)+(blue).toString(16); //changing block background color by converting RGB to hexadecimal
+    data.push({redVal: red,blueVal: blue,greenVal: green});
 }
 
-function checkResult()
+// function checkResult()
+// {
+// TO DO
+// }
+
+for(var i=0 ; i<6; i++)
 {
-    if(aRed===red && aBlue===blue && aGreen===green)
-        {
-            result.textContent="Wrong";
-        }
+    changeBackground(blocks[i]);
+    // blocks[i].addEventListener("click", checkResult);
 }
 
-
-
-for(var i=0 ; i<7; i++)
-    {
-        changeBackground(blocks[i]);
-        blocks[i].addEventListener("click", checkResult);
-    }
-
+rField.textContent=data[correctColor].redVal;
+bField.textContent=data[correctColor].blueVal;
+gField.textContent=data[correctColor].greenVal;
